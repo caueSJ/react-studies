466622966875a9133242f10896f62cffe6d86e80
Caue SJ <caue.silvaa@gmail.com>
2020-12-08 16:59:31 -0300

    Changed to class based + Passing method references between components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,46 +1,58 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Albert', age: 20 },
       { name: 'Brayan', age: 22 },
       { name: 'Charee', age: 24 }
-    ]
-  });
+    ],
+    otherState: 'Other value here!'
+  }
 
-  const [otherState, setOtherState] = useState('Other value here!');
-
-  const switchNameHandler = (newName) => {
-    setPersonsState({
+  switchNameHandler = (newName) => {
+    this.setState({
       persons: [
         { name: newName, age: 21 },
         { name: 'Brayanão', age: 23 },
         { name: 'Chareezuda', age: 25 }
       ]
     });
-  } 
+  }
 
-  return (
-    <div className="App">
-      <h1>React App</h1>
-      <button onClick={() => this.switchNameHandler() }>
-        Switch Names
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Albert ', age: 21 },
+        { name: event.target.value, age: 23 },
+        { name: 'Chareezuda', age: 25 }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React App</h1>
+        <button onClick={() => this.switchNameHandler.bind('Novo nome')}>
+          Switch Names
       </button>
-      <Person
-        name={personsState.persons[0].name}
-        age={personsState.persons[0].age} />
-      <Person
-        name={personsState.persons[1].name}
-        age={personsState.persons[1].age}
-        click={this.switchNameHandler.bind(this, 'Nominado')} />
-      <Person
-        name={personsState.persons[2].name}
-        age={personsState.persons[2].age} />
-    </div>
-  );
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Novo nominho')}
+          changed={this.nameChangedHandler} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,9 +1,14 @@
+// This component is functional based, a dumb component, stateless
 import React from 'react';
 
 const person = props => {
-    return (
-        <p>I'm {props.name} and I have {props.age} years old! </p>
-    );
+  return (
+    <div>
+      <p onClick={props.click}>I'm {props.name} and I have {props.age} years old! </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
 }
 
 export default person;
